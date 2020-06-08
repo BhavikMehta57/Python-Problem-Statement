53745f8784f927cd8ba6114b35afbbbf0a3bffbf
BhavikMehta57 <66597816+BhavikMehta57@users.noreply.github.com>
2020-06-08 13:33:52 +0530

    Update Question1.py
    Read two integers from STDIN and print three lines where:
    ● The first line contains the sum of the two numbers.
    ● The second line contains the difference between the two numbers (first -
    second).
    ● The third line contains the product of the two numbers.

--- Question1.py
+++ Question1.py
@@ -1,11 +1,9 @@
 a = int(input())                          #Read two integers
 b = int(input())
-
 if 1<=a<=10**10 and 1<=b<=10**10:         #constraints
  sum = a+b
  difference = a-b
  product = a*b
-
  print (sum)                              #The first line contains the sum of the two numbers.
  print (difference)                       #The second line contains the difference of the two numbers (first - second).
  print (product)                          #The third line contains the product of the two numbers.
