fb4865b5ab8fc2f23664b38afae03a0125fdcc3e
BhavikMehta57 <66597816+BhavikMehta57@users.noreply.github.com>
2020-06-08 15:22:06 +0530

    Create Question2.py
    We add a Leap Day on February 29, almost every four years. The leap day is an extra,
    or intercalary day and we add it to the shortest month of the year, February.
    In the Gregorian calendar three criteria must be taken into account to identify leap
    years:
    ● The year can be evenly divided by 4, is a leap year, unless:
    ● The year can be evenly divided by 100, it is NOT a leap year, unless:
    ● The year is also evenly divisible by 400. Then it is a leap year.
    This means that in the Gregorian calendar, the years 2000 and 2400 are leap years,
    while 1800, 1900, 2100, 2200, 2300 and 2500 are NOT leap years.

---
+++ Question2.py
@@ -0,0 +1,14 @@
+def is_leap(year):
+    leap = 'It is not a Leap year'
+
+    # Write your logic here
+    if year % 400 == 0:
+        leap = 'It is a Leap year'
+    elif year % 100 == 0:
+        leap = 'It is not a Leap year'
+    elif year % 4 == 0:
+        leap = 'It is a Leap year'
+
+    return leap
+year = int(input())
+print (is_leap(year))
